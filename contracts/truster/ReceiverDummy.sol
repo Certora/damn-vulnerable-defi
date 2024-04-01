3822c999211a383bd4af21d47648970feb7cc4a0
niv vaknin <niv@certora.com>
2024-04-01 12:00:20 +0300

    Address nurit comment

--- contracts/truster/ReceiverDummy.sol
+++ contracts/truster/ReceiverDummy.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.8.20;
 
-import "./FixedTrusterLenderPool.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract ReceiverDummy is IReceiver{
+contract ReceiverDummy{
     
     function execute(bytes memory data) external returns (bool, uint) {
         return (true, 0);
